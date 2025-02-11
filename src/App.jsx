e3dd92ba6f684516ac0987ecac8ba0bf8b0d70b6
akash.agarwal <1998akash28@gmail.com>
2025-02-11 17:06:10 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import ColorPaletteGenerator from "./components/ColorPaletteGenerator";
+
+function App() {
+  return (
+    <>
+      <ColorPaletteGenerator />
+    </>
+  );
+}
+
+export default App;
